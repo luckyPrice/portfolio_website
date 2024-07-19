9a80c09bf7c27f4187f8cbf75f9ddae0c9d31c5b
luckyPrice <luckyprice1103@naver.com>
2024-07-19 21:54:00 +0900

    second commit

---
+++ main.tf
@@ -0,0 +1,75 @@
+#create s3 bucket
+resource "aws_s3_bucket" "luckybucket" {
+  bucket = var.bucketname
+}
+
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.luckybucket.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.luckybucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.example,
+    aws_s3_bucket_public_access_block.example,
+  ]
+
+  bucket = aws_s3_bucket.luckybucket.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_object" "index" {
+  bucket = aws_s3_bucket.luckybucket.id
+  key    = "index.html"
+  source = "index.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error" {
+  bucket = aws_s3_bucket.luckybucket.id 
+  key    = "error.html"
+  source = "error.html"
+  acl = "public-read"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "profile" {
+  bucket = aws_s3_bucket.luckybucket.id 
+  key    = "profile.jpeg"
+  source = "profile.jpeg"
+  acl = "public-read"
+}
+
+resource "aws_s3_object" "logofolder" {
+  for_each = fileset("./logo/", "**")
+  bucket = aws_s3_bucket.luckybucket.id
+  key    = "/logo/${each.value}"
+  source = "./logo/${each.value}"
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.luckybucket.id
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "error.html"
+  }
+
+  
+  depends_on = [ aws_s3_bucket_acl.example ]
+}
